686a91217bfd1b7c78e6dba3d2631a5b86b6ed55
vladimir92ua <vladimir.globchastyi@logytext.com>
2025-03-04 18:14:15 +0200

    Changed components

--- src/forms/LoginForm/LoginForm.tsx
+++ src/forms/LoginForm/LoginForm.tsx
@@ -1,6 +1,9 @@
 import { useForm } from 'react-hook-form';
 import { LoginFormDate, LoginFormProps } from './LoginForm.types';
 import { validators } from './LoginForm.data';
+import { H3 } from '@/components/Typography/Typography.component';
+import InputField from '@/components/Inputs/InputField';
+import Button from '@/components/Button';
 
 const LoginForm = ({ errorMessage, onSubmit }: LoginFormProps) => {
   const {
@@ -15,43 +18,37 @@ const LoginForm = ({ errorMessage, onSubmit }: LoginFormProps) => {
   });
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)} className='p-6'>
-      <h3 className='formTitle'>Log in</h3>
+    <form onSubmit={handleSubmit(onSubmit)} className='border border-gray-100'>
+      <div className='w-full border-b border-gray-100'>
+        <H3 className='px-5 py-6'>Log in</H3>
+      </div>
 
-      <div className='w-full flex flex-col gap-1'>
-        <label htmlFor='email' className='text-sm font-medium'>
-          Username
-        </label>
-        <input
-          className='styledInput'
+      <div className='w-full flex flex-col gap-6 p-6'>
+        <InputField
+          placeholder='Enter email...'
+          label='Email'
+          error={!!errors?.email}
+          helperText={errors.email?.message}
           id='email'
           {...register('email', validators.email)}
         />
-        {errors.email && (
-          <span className='errorText text-sm'>{errors.email.message}</span>
-        )}
-      </div>
 
-      <div className='w-full flex flex-col gap-1'>
-        <label htmlFor='password' className='text-sm font-medium'>
-          Password
-        </label>
-        <input
+        <InputField
+          placeholder='Enter password...'
+          label='Password'
+          error={!!errors?.password}
+          helperText={errors.password?.message}
           id='password'
-          className='styledInput'
           type='password'
           {...register('password', validators.password)}
         />
-        {errors.password && (
-          <span className='errorText text-sm'>{errors.password.message}</span>
-        )}
+
+        {errorMessage && <span className='errorText'>{errorMessage}</span>}
+
+        <Button className='w-full' disabled={!isDirty} type='submit'>
+          Submit
+        </Button>
       </div>
-
-      {errorMessage && <span className='errorText'>{errorMessage}</span>}
-
-      <button disabled={!isDirty} className='mt-3 w-full' type='submit'>
-        Submit
-      </button>
     </form>
   );
 };
