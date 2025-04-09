33fc550b473d6e75f8ccb7a6606354b9e6db606e
vladimir92ua <vladimir.globchastyi@logytext.com>
2025-04-09 14:56:52 +0300

    Added main route

---
+++ src/routes/route.tsx
@@ -0,0 +1,26 @@
+import Avatar from '@/components/Avatar';
+import { H1 } from '@/components/Typography/Typography.component';
+import { createFileRoute, useNavigate } from '@tanstack/react-router';
+
+export const Route = createFileRoute('/')({
+  beforeLoad: ({ navigate }) => {
+    return navigate({ to: '/user' });
+  },
+  component: RouteComponent,
+});
+
+function RouteComponent() {
+  const navigate = useNavigate();
+
+  return (
+    <div className='w-full h-screen flex justify-center items-center'>
+      <div
+        className='flex flex-col items-center cursor-pointer py-14 px-6 hover:rounded-full hover:border-medium hover:border-border-focus'
+        onClick={() => navigate({ to: '/user', from: '/' })}
+      >
+        <Avatar src='flyChat.svg' size='xl' alt='FlyChatLogo' />
+        <H1>Welcome to the FlyChat!</H1>
+      </div>
+    </div>
+  );
+}
